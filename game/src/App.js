fae4177c0498005bb38a9fb45f813177f4b470a6
Christie <cholvie@gmail.com>
2018-06-23 00:30:04 -0400

    h

---
+++ game/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Counter from "./components/Counter";
+import "./App.css";
+
+const App = () => <Counter />;
+
+export default App;
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends
+  };
+
+  removeFriend = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const friends = this.state.friends.filter(friend => friend.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ friends });
+  };
